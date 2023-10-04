eaba5bb20de94b141b8b5d2062c739038afbc69f
Haining Pan <jack_pan1994@126.com>
2023-10-03 23:49:16 -0400

    update db_tensor.py

--- db_tensor.py
+++ db_tensor.py
@@ -22,9 +22,10 @@ def run_tensor(gpu,complex128):
 
     st0=time.time()
     # ct=CT_tensor(L=18,gpu=True,seed=list(range(200)),x0=None,ancilla=False,history=False,complex128=True)
-    ct=CT_tensor(L=16,gpu=gpu,seed=0,x0=None,ancilla=False,history=False,complex128=complex128,ensemble=2000)
-    for _ in range(2*ct.L**2):
-        ct.random_control(0.0,0)
+    ct=CT_tensor(L=16,gpu=gpu,seed=0,x0=None,ancilla=True,history=False,complex128=complex128,ensemble=2000)
+    init_timestamp=time.time()
+    for _ in range(ct.L**2//2):
+        ct.random_control(1,0)
         torch.cuda.empty_cache()
     evo_timestamp=time.time()
     _=ct.order_parameter()
@@ -34,10 +35,10 @@ def run_tensor(gpu,complex128):
     _=ct.tripartite_mutual_information(np.arange(ct.L//4),np.arange(ct.L//4)+ct.L//4,np.arange(ct.L//4)+ct.L//4*2)
     TMI_timestamp=time.time()
     peak_memory_MB = torch.cuda.max_memory_allocated()/ (1024 ** 2)
-    return evo_timestamp-st0,OP_timestamp-evo_timestamp,EE_timestamp-OP_timestamp,TMI_timestamp-EE_timestamp,peak_memory_MB, ct.vec.numel()*ct.vec.element_size()/1024**2
-
+    return init_timestamp-st0,evo_timestamp-st0,OP_timestamp-evo_timestamp,EE_timestamp-OP_timestamp,TMI_timestamp-EE_timestamp,peak_memory_MB, ct.vec.numel()*ct.vec.element_size()/1024**2
+ 
 for gpu in [torch.cuda.is_available(),]:
     for complex128 in [False,]:
         print(f'GPU:{gpu}, complex128: {complex128}')
-        print('Evolution Time:{:.2f} s\nOP Time:{:.2f} s\nEE Time:{:.2f} s\nTMI Time:{:.2f} s\nPeak GPU RAM:{:.2f} MB\nTensor GPU Ram:{:.2f} MB'.format(*run_tensor(gpu,complex128)))
+        print('Initialization Time: {:.2f} s\nEvolution Time:{:.2f} s\nOP Time:{:.2f} s\nEE Time:{:.2f} s\nTMI Time:{:.2f} s\nPeak GPU RAM:{:.2f} MB\nTensor GPU Ram:{:.2f} MB'.format(*run_tensor(gpu,complex128)))
 

15b62f6809bb8f025bdeee9220199573d31c4657
Haining Pan <hnpan@terpmail.umd.edu>
2024-01-12 01:29:34 -0500

    update bricklayer.py with the third minimal model : zigzag

--- rqc/bricklayer.py
+++ rqc/bricklayer.py
@@ -2,9 +2,10 @@ import scipy.sparse as sp
 import numpy as np
 from .utils import U, Haar_state, dec2bin
 from functools import lru_cache
+from fractions import Fraction
 
 class bricklayer:
-    def __init__(self,L,store_vec=False,store_op=False,store_prob=False,seed=None,seed_vec=None,seed_C=None,x0=None,_eps=1e-10,add_x=0,debug=False,feedback=False):
+    def __init__(self,L,store_vec=False,store_op=False,store_prob=False,seed=None,seed_vec=None,seed_C=None,x0=None,_eps=1e-10,add_x=0,debug=False,feedback=False,xj=None):
         self.L=L
         self.store_vec=store_vec
         self.store_op=store_op
@@ -21,6 +22,7 @@ class bricklayer:
         self.add_x=add_x
         self.debug=debug
         self.feedback=feedback
+        self.xj=xj
 
     def _initialize_vector(self):
         if self.x0 is not None:
@@ -56,16 +58,49 @@ class bricklayer:
         vec=self.adder(i)@vec
         return vec
 
+    def _right_shift(self,d,i):
+        ''' make i-th bit in the output the leading bit'''
+        d_bin=bin(d)[2:].zfill(self.L)
+        return int(d_bin[-i:]+d_bin[:-i],2)
+
+    def _left_shift(self,d,i):
+        ''' make i-th bit in the input the leading bit'''
+        d_bin=bin(d)[2:].zfill(self.L)
+        return int(d_bin[i:]+d_bin[:i],2)
+
+
     @lru_cache(maxsize=None)
     def adder(self,i):
+        ''' if xj is None, then use add_x as a universal adder, 
+        otherwise, use xj as the adder, which is a outcome dependent adder, currently support only xj={Fraction(1,3),Fraction(2,3)}'''
         old_idx=np.arange(2**(self.L))
-        add_x_bin=bin(self.add_x)[2:].zfill(self.L)
-        add_x= int(add_x_bin[-i:]+add_x_bin[:-i],2)
-        # print(add_x)
-        new_idx=(old_idx+add_x)%2**self.L
+        if self.xj is None:
+            add_x=self._right_shift(self.add_x,i)
+            # print(add_x)
+            new_idx=(old_idx+add_x)%2**self.L
+        else:
+            if self.xj==set([Fraction(1,3),Fraction(2,3)]):
+                int_1_6=dec2bin(Fraction(1,6), self.L)|1
+                int_1_3=dec2bin(Fraction(1,3), self.L)
+                # add_x_1_6=self._right_shift(int_1_6,i)
+                # add_x_1_3=self._right_shift(int_1_3,i)
+                add_x=np.array([int_1_6]*2**(self.L-2)+[int_1_3]*2**(self.L-2)+[int_1_6]*2**(self.L-2)+[int_1_3]*2**(self.L-2))
+                new_idx=(old_idx+add_x)%2**self.L
+                mask_1=(new_idx&(1<<self.L-1) == (1<<self.L-1)) & (new_idx&(1<<2) == (0)) & (new_idx&(1) == (1))
+                mask_2=(new_idx&(1<<self.L-1) == (0)) & (new_idx&(1<<2) == (1<<2)) & (new_idx&(1) == (0))
+                new_idx[mask_1+mask_2]=new_idx[mask_1+mask_2]^(0b10)
+
+                # Recover to real basis
+                old_idx=[self._right_shift(d=idx,i=i) for idx in old_idx]
+                new_idx=[self._right_shift(d=idx,i=i) for idx in new_idx]
+
+                # print(np.vstack([old_idx,new_idx]))
+                # print(old_idx_2)
+                # print(new_idx_2)
+                # print(new_idx)
+        
         ones=np.ones(2**(self.L))
         return sp.coo_matrix((ones,(new_idx,old_idx)),shape=(2**self.L,2**self.L))
-    
     def inner_prob(self,vec,n,i):
         '''n: outcome 0 or 1, i: position'''
         if vec.ndim != (2,)*self.L:
@@ -112,7 +147,7 @@ class bricklayer:
     
     def P_layer(self,vec,p_proj):
         '''apply one layers of P gates'''
-        for i in range(self.L):
+        for i in range(self.L)[::-1]:
             if self.rng_C.random()<p_proj:
                 p_0=self.inner_prob(vec,i=i,n=0)
                 if self.rng.random()<p_0:
@@ -124,7 +159,7 @@ class bricklayer:
 
     def C_layer(self,vec,p_proj):
         ''' apply one layer of Control (C) gates, C=U R, where R is the measurement with feedback'''
-        for i in range(self.L):
+        for i in range(self.L)[::-1]:
             if self.rng_C.random()<p_proj:
                 p_0=self.inner_prob(vec,i=i,n=0)
                 if self.rng.random()<p_0:
@@ -132,10 +167,37 @@ class bricklayer:
                 else:
                     vec=self.R_tensor(vec,n=1,i=i)
                 vec=self.normalize(vec)
-
                 vec=self.adder_cpu(vec,i)
-
         return vec
+
+    def U_layer_staircase(self,vec,start):
+        '''apply the unitary in a staircase fashion, starting with (-1,0), to (L-2,L-1)'''
+        for idx in range(start,start+self.L):
+            i=idx%self.L
+            print(f'Apply U to {i,(i+1)%self.L}')
+            vec=self.U_tensor(vec,i%self.L,rng=self.rng_C)
+            # print(vec.flatten())
+        return vec,(i+1)%self.L
+    
+    def C_layer_binomial(self,vec,p_proj,start):
+        '''apply the control map from right to left for n times, where n obeys binomial distribution'''
+        n=self.rng_C.binomial(self.L,p_proj)
+        # for i in range(self.L-n,self.L)[::-1]:
+        if n>0:
+            for idx in range(start,start-n,-1):
+                i=idx%self.L
+                p_0=self.inner_prob(vec,i=i,n=0)
+                outcome= 0 if self.rng.random()<=p_0 else 1
+                print(f'Apply control to {i} with {outcome}')
+                vec=self.R_tensor(vec,n=outcome,i=i)
+                vec=self.normalize(vec)
+                # print(np.where(vec.flatten()))
+                vec=self.adder_cpu(vec,i)
+                # print(np.where(vec.flatten()))
+                # print(vec.flatten())
+            return vec,(i-1)%self.L
+        else:
+            return vec,start
 
 
     def random_projection(self,p_proj):
@@ -156,7 +218,14 @@ class bricklayer:
         vec=self.U_layer(vec,even=False)
         vec=self.C_layer(vec,p_proj)
         self.update_history(vec,)
-
+    
+    def random_projection_3(self,p_proj,start):
+        ''' the control map should be applied successively, from left to right, without skipping any qubit'''
+        vec=self.vec_history[-1].copy()
+        vec,start=self.U_layer_staircase(vec,start=start)
+        vec,start=self.C_layer_binomial(vec,p_proj,start=start)
+        self.update_history(vec,)
+        return start
 
     def update_history(self,vec):
         if vec is not None:
@@ -206,6 +275,7 @@ class bricklayer:
             S_AC=self.von_Neumann_entropy_pure(np.concatenate([subregion_A,subregion_C]),vec=vec,n=n,threshold=threshold)
             S_BC=self.von_Neumann_entropy_pure(np.concatenate([subregion_B,subregion_C]),vec=vec,n=n,threshold=threshold)
             S_ABC=self.von_Neumann_entropy_pure(np.concatenate([subregion_A,subregion_B,subregion_C]),vec=vec,n=n,threshold=threshold)
+            # print(S_A+ S_B , S_C,S_AB,S_AC,S_BC,S_ABC)
             return S_A+ S_B + S_C-S_AB-S_AC-S_BC+S_ABC
 
     def half_system_entanglement_entropy(self,vec=None,selfaverage=False,n=1,threshold=1e-10):
@@ -269,6 +339,41 @@ class bricklayer:
         else:
             return np.log(np.sum(S_pos2**n))/(1-n)
 
+    def order_parameter(self,vec=None,kind='AFM'):
+        if vec is None:
+            vec=self.vec_history[-1].copy()
+        if kind=='AFM':
+            O=self.ZZ_tensor(vec)
+        else:
+            raise ValueError(f'kind {kind} not supported')
+        return O.real
+    def ZZ_tensor(self,vec):
+        """Calculate the order parameter for Neel state. The order parameter is defined as \sum_{i=0..L-1} <Z_iZ_{i+1}>, where Z_i is the Pauli Z matrix at site i.
+
+        Parameters
+        ----------
+        vec : numpy.array, shape=(2**L_T,) or (2,)*L_T
+            state vector
+
+        Returns
+        -------
+        float
+            order parameter for Neel state
+        """
+        if vec.ndim != (2,)*self.L:
+            vec=vec.reshape((2,)*self.L)
+        rs=0
+        for i in range(self.L):
+            for zi in range(2):
+                for zj in range(2):
+                    # idx_list=[slice(None)]*self.L
+                    # idx_list[i],idx_list[(i+1)%self.L]=zi,zj
+                    inner_prod=self.inner_prob(vec, i=[i,(i+1)%self.L],n=[zi,zj])
+                    exp=1-2*(zi^zj) # expectation-- zi^zj is xor of two bits which is only one when zi!=zj
+                    # vec_i=vec[tuple(idx_list)].flatten()
+                    # rs+=vec_i.conj()@vec_i*exp
+                    rs+=inner_prod*exp
+        return -rs/self.L
 
 
         

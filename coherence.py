0f9b90db9b3e15810dc0881a0b977225959e95bb
Haining Pan <hnpan@terpmail.umd.edu>
2024-06-20 13:33:09 -0400

    udpate coherence and find it failed...

--- coherence.py
+++ coherence.py
@@ -43,22 +43,42 @@ def save_reduced_dm_swap(f_0, L,s_max=2000,idx_max=21,hdf5=False):
         with open(f'C_av_{L}_tt.pickle','wb') as f:
             pickle.dump({'red_dm':red_dm_list,'red_dm_per':red_dm_per_list},f)
 
-def save_reduced_dm_T(f_0, L,s_max=2000,idx_max=21,hdf5=False):
+def save_reduced_dm_T(f_T, L,T_max=None, idx_max=21,hdf5=False):
     """ \sum_m p_m  C(\rho_m)"""
-
-    red_dm_list=np.zeros((idx_max,L+1,L+1),dtype=np.float64)
-    red_dm_per_list=np.zeros((idx_max,L+1,L+1),dtype=np.float64)
+    if T_max is None:
+        T_max=1+2*L**2
+    red_dm_list=np.zeros((idx_max,T_max,L+1,L+1),dtype=np.float64)
+    red_dm_per_list=np.zeros((idx_max,T_max,L+1,L+1),dtype=np.float64)
     for idx in tqdm(range(idx_max)):
-        red_dm=[get_reduced_dm(get_rho_av(f_0,L,idx,s)) for s in range(s_max)]
-        red_dm_list[idx]=np.mean(red_dm,axis=0)
-        red_dm_per_list[idx]=get_reduced_dm_per_basis(red_dm_list[idx])
+        red_dm=np.array([get_reduced_dm(get_rho_av_T(f_T,L=L,i=idx,T=T)) for T in range(T_max)])
+        red_dm_list[idx]=(red_dm)
+        red_dm_per_list[idx]=np.array([get_reduced_dm_per_basis(rho) for rho in red_dm_list[idx]])
     if hdf5:
-        with h5py.File(f'C_av_{L}.hdf5','w') as f:
+        with h5py.File(f'rho_T_av_{L}.hdf5','w') as f:
             f.create_dataset('red_dm',data=red_dm_list)
             f.create_dataset('red_dm_per',data=red_dm_per_list)
     else:
-        with open(f'C_av_{L}.pickle','wb') as f:
+        with open(f'rho_T_av_{L}.pickle','wb') as f:
             pickle.dump({'red_dm':red_dm_list,'red_dm_per':red_dm_per_list},f)
+
+def save_reduced_dm_T_seed(f_T, L,seed_range, T_list=None, i_list=None, hdf5=False,bootstrap=False,rng=None,save=True):
+    if T_list is None:
+        T_list=range(0,1+2*L**2)
+    if i_list is None:
+        i_list=range(21)
+
+    red_dm_list=np.zeros((len(i_list),len(T_list),L+1,L+1),dtype=np.float64)
+    red_dm_per_list=np.zeros((len(i_list),len(T_list),L+1,L+1),dtype=np.float64)
+    for i_idx,i in (enumerate(i_list)):
+    # for i_idx,i in tqdm(enumerate(i_list)):
+        red_dm=np.array([get_reduced_dm(get_rho_av_T_seed(f_T,L=L,i=i,T=T,seed_range=seed_range,bootstrap=bootstrap,rng=rng)) for T_idx,T in enumerate(T_list)])
+        red_dm_list[i_idx]=(red_dm)
+        red_dm_per_list[i_idx]=np.array([get_reduced_dm_per_basis(rho) for rho in red_dm_list[i_idx]])
+    if save:
+        with open(f'rho_T_av_{L}_all.pickle','wb') as f:
+                pickle.dump({'red_dm':red_dm_list,'red_dm_per':red_dm_per_list},f)
+    else:
+        return red_dm_list, red_dm_per_list
 
 def l1_coherence(rho,k,normalization=False,average=False):
     L=len(rho.shape)//2
@@ -108,14 +128,50 @@ def get_rho_av(f_0,L,i,s=None):
         return rho_av
     else:
         wf=torch.from_numpy(f_0[L][f'wf_{L}'][i,0,0,...,s,0])
-        rho=torch.abs(contract(wf,list(range(L)),np.conj(wf),list(range(L,2*L)),list(range(2*L))))
+        rho=torch.abs(contract(wf,list(range(L)),torch.conj(wf),list(range(L,2*L)),list(range(2*L))))
         return rho
         
-def get_rho_av_T(f,L,i,T):
-    wf=torch.from_numpy(f[L][f'wf_{L}'][i,0,T,...,:,0])
-    rho_av=np.abs(contract(wf,np.r_[np.arange(0,L),2*L],np.conj(wf),np.r_[np.arange(L,2*L),2*L],np.arange(2*L))/f[L][f'wf_{L}'].shape[-2])
-    return rho_av
+def get_rho_av_T(f,L,i,T,s=None):
+    if s is None:
+        wf=torch.from_numpy(f[L][f'wf_{L}'][i,0,T,...,:,0])
+        # rho_av=(contract(wf,np.r_[np.arange(0,L),2*L],np.conj(wf),np.r_[np.arange(L,2*L),2*L],np.arange(2*L))/f[L][f'wf_{L}'].shape[-2])
+        rho_av=torch.abs(contract(wf,np.r_[np.arange(0,L),2*L],np.conj(wf),np.r_[np.arange(L,2*L),2*L],np.arange(2*L))/f[L][f'wf_{L}'].shape[-2])
+        return rho_av
+    else:
+        wf=torch.from_numpy(f[L][f'wf_{L}'][i,0,T,...,s,0])
+        rho=torch.abs(contract(wf,list(range(L)),torch.conj(wf),list(range(L,2*L)),list(range(2*L))))
+        return rho
 
+def get_rho_av_T_seed(f,L,i,T,seed_range, bootstrap=False,rng=None):
+    for seed in seed_range:
+        if seed == seed_range[0]:
+            wf=torch.from_numpy(f[seed][f'wf_{L}'][i,0,T,...,:,0])
+        else:
+            wf=torch.cat((wf,torch.from_numpy(f[seed][f'wf_{L}'][i,0,T,...,:,0])),dim=-1)
+    if bootstrap is not False:
+        wf=resample_last_axis(wf,bootstrap,rng)
+    rho_av=torch.abs(contract(wf,np.r_[np.arange(0,L),2*L],np.conj(wf),np.r_[np.arange(L,2*L),2*L],np.arange(2*L))/wf.shape[-1])
+    return rho_av
+        
+def resample_last_axis(tensor, num_samples, rng=None):
+    """
+    Resamples the last axis of a tensor with replacement.
+
+    Parameters:
+    tensor (torch.Tensor): The input tensor.
+    num_samples (int): The number of samples to draw along the last axis.
+
+    Returns:
+    torch.Tensor: The resampled tensor.
+    """
+    # Generate random indices with replacement
+    if rng is not None:
+        torch.manual_seed(rng)
+
+    indices = torch.randint(0, tensor.size(-1), (num_samples,))
+    # Resample the tensor along the last axis
+    resampled_tensor = tensor[..., indices]
+    return resampled_tensor
 
 def get_reduced_dm(rho):
     L=len(rho.shape)//2

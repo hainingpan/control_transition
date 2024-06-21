dfdad2e5385506114e76a5fb2b19caae016c1cb9
Haining Pan <hnpan@terpmail.umd.edu>
2024-06-20 20:57:18 -0400

    update coherence

--- coherence_save.py
+++ coherence_save.py
@@ -7,6 +7,10 @@ def run(L):
     save_reduced_dm(f_0,L=L,internal_coherence=True)
     # save_reduced_dm_swap(f_0,L=L)
 
+# def run2(L):
+#     red_dm_list_map,red_dm_per_list_map=resample(f_T_s,L=8,T_list=T_list,i_list=[0],ensemble_size=20,bootstrap_size_list=bootstrap_size_list,seed_max=5,internal_coherence=True)
+#     with open('rho_ave')
+
 if __name__  == "__main__":
     for L in [16]:
     # for L in [8,]:

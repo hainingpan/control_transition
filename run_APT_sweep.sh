623d85bdca7217b0df9584175ae28b8d8036cddc
Haining Pan <hnpan@terpmail.umd.edu>
2024-10-26 20:19:21 -0400

    update APT

---
+++ run_APT_sweep.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --partition=main
+#SBATCH --requeue
+#SBATCH --time=5:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=20
+#SBATCH --mem=8000
+#SBATCH --cpus-per-task=1
+#SBATCH --output=ARRARIDX.out
+#SBATCH --error=ARRARIDX.err
+ 
+cd $PWD
+
+module purge
+
+module load singularity
+
+# module load slurm
+
+PARAMS_FILE="$PWD/params_APT.txt"
+read -r es0 es1 esC0 esC1 p_m L <<< $(sed -n "ARRARIDXp" $PARAMS_FILE)
+
+echo $es0 $es1 $esC0 $esC1 $p_m $L
+# srun singularity exec /scratch/hp636/python.sif mpirun -np 20 python -m mpi4py.futures run_APT_C_m.py -es $es0 $es1 -es_C $esC0 $esC1 -p_m $p_m $p_m 1 -p_f 1 1 1 -L $L
+srun singularity exec /scratch/hp636/python.sif python -m mpi4py.futures run_APT_C_m.py -es $es0 $es1 -es_C $esC0 $esC1 -p_m $p_m $p_m 1 -p_f 1 1 1 -L $L
+
---
+++ run_pytorch_sweep2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu
+##SBATCH --nodelist=gpu015,gpu016
+#SBATCH --exclude=cuda00[1-8],gpuc00[1-2],pascal0[01-10],volta0[01-03],gpu0[05-14],gpu0[17-18]
+#SBATCH --time=20:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=8000
+#SBATCH --cpus-per-task=1
+#SBATCH --output=ARRARIDX.out
+#SBATCH --error=ARRARIDX.err
+##SBATCH --array=1-83%16
+# Your commands here
+ 
+cd $PWD
+
+module purge
+
+module load singularity
+
+PARAMS_FILE="$PWD/params_1_3_anc_T.txt"
+# PARAMS_FILE="$PWD/params_0_anc_ent.txt"
+# PARAMS_FILE="$PWD/params_1-3,2-3,-1-3.txt"
+# read -r seed p_ctrl p_proj L1 L2 xj es ancilla <<< $(sed -n "${SLURM_ARRAY_TASK_ID}p" $PARAMS_FILE)
+read -r seed p_ctrl p_proj L1 L2 xj es ancilla <<< $(sed -n "ARRARIDXp" $PARAMS_FILE)
+
+echo $seed  $p_ctrl $p_proj $L1 $L2 "$xj" $es $ancilla
+# for run_pytorch.py
+srun singularity exec --nv /scratch/hp636/pytorch.sif python run_pytorch_T.py -seed $seed -p_ctrl $p_ctrl $p_ctrl 1 -p_proj $p_proj $p_proj 1 -L $L1 $L2 2 -xj "$xj" -es $es $ancilla
+
+# # for run_pytorch_interpolation.py
+# srun singularity exec --nv /scratch/hp636/pytorch.sif python run_pytorch_interpolation.py -seed $seed -p_ctrl $p_ctrl $p_ctrl 1 -p_global $p_global $p_global 1 -L $L1 $L2 2 -xj "$xj" -es $es $ancilla -p_proj 0. 0. 1
+
+# for run_pytorch_sv.py, singular value
+# srun singularity exec --nv /scratch/hp636/pytorch.sif python run_pytorch_sv.py -seed $seed -p_ctrl $p_ctrl $p_ctrl 1 -p_proj $p_proj $p_proj 1 -L $L1 $L2 2 -xj "$xj" -es $es $ancilla --complex128

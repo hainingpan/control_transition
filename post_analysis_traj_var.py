cd8a5deb8d5e2b6ad414645593dfc92d71d9f826
Haining Pan <hnpan@terpmail.umd.edu>
2024-10-28 14:05:10 -0400

    update post_analysis_traj_var

--- post_analysis_traj_var.py
+++ post_analysis_traj_var.py
@@ -1,4 +1,5 @@
 from plot_utils import *
+from tqdm import tqdm
 L=12
 sC_list=range(500)
 sm_list=range(500)
@@ -13,7 +14,7 @@ for fixed_params,vary_params in params_list:
         fixed_params=fixed_params,
         vary_params=vary_params,
         fn_template='APT_En({es0},{es0+per_es0})_EnC(0,500)_pm({p_m:.2f},{p_m:.2f},1)_pf(1.00,1.00,1)_L{L}_T.pickle',
-        fn_dir_template='APT_T',
+        fn_dir_template='/home/jake/Data/APT_T',
         input_params_template='{p:.3f} {L} {seed} {ancilla}',
         load_data=load_pickle,
         filename=None,
@@ -28,15 +29,16 @@ def trajvar(df,L,p_m,sC):
     data=df.xs(L,level='L').xs(p_m,level='p_m').xs(1,level='p_f').xs(sC,level='sC')
 
     # single=[data.xs(sm,level='sm').loc['DW1']['observations'] for sm in range((params_list[0][1]['sm']).shape[0])]
-    single=[data.xs(sm,level='sm').loc['O']['observations'] for sm in sm_list]
+    # single=[data.xs(sm,level='sm').loc['O']['observations'] for sm in sm_list]
+    single = np.vstack(data.xs('O',level='Metrics')['observations'])
 
-    return np.array(single).var(axis=0)
+    return single.var(axis=-1)
 
 sC_traj_var={}
 for p in p_m_list:
     print(p)
     sC_traj_var[(p,L)]=[]
-    for sC in range(500):
+    for sC in tqdm(range(500)):
         sC_traj_var[(p,L)].append(trajvar(data_APT,L=L,p_m=p,sC=sC))
     sC_traj_var[(p,L)]=np.array(sC_traj_var[(p,L)])
 

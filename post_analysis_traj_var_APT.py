fa09f7f78bbbd9f321cc750937dba9c52115a7e9
Haining Pan <hnpan@terpmail.umd.edu>
2024-11-27 10:37:46 -0500

    Refactor post_analysis_traj_var.py for improved readability and performance

--- post_analysis_traj_var_APT.py
+++ post_analysis_traj_var_APT.py
@@ -7,20 +7,18 @@ sm_list=range(500)
 p_m_list=[0.07,0.08,0.085,0.087,0.089,0.09,0.091,0.093,0.095,0.1,0.105,0.11,]
 # p_m_list=[0.09]
 
-def trajvar(df,L,p_m,sC):
-    data=df.xs(L,level='L').xs(p_m,level='p_m').xs(1,level='p_f').xs(sC,level='sC')
-
-    single = np.vstack(data.xs('O',level='Metrics')['observations'])
-
-    return single.var(axis=0)
+def trajvar(df,sC):
+    data=df.xs(sC,level='sC')
+    return data.var()
+    
 sC_traj_var={}
-data_APT_dict={'fn':set()}
 for p_m in p_m_list:
+    data_APT_dict={'fn':set()}
     
     params_list=[
-    # ({'L':12,'per_esC0':50},{'esC0':range(0,2000,50),'p_m':[p_m]}),
+    ({'L':12,'per_esC0':50},{'esC0':range(0,2000,50),'p_m':[p_m]}),
     # ({'L':14,'per_esC0':25},{'esC0':range(0,2000,25),'p_m':[p_m]}),
-    ({'L':16,'per_esC0':10},{'esC0':range(0,2000,10),'p_m':[p_m]}),
+    # ({'L':16,'per_esC0':10},{'esC0':range(0,2000,10),'p_m':[p_m]}),
     # ({'L':18,'per_esC0':5},{'esC0':range(0,2000,5),'p_m':[p_m]}),
     # ({'L':20,'per_esC0':2},{'esC0':range(0,2000,2),'p_m':[p_m]}),
     ]
@@ -33,8 +31,8 @@ for p_m in p_m_list:
             vary_params=vary_params,
             fn_template='APT_EnC({esC0},{esC0+per_esC0})_Enm(0,500)_pm({p_m:.3f},{p_m:.3f},1)_pf(1.000,1.000,1)_L{L}_Tf.pickle',
             # fn_dir_template='APT_Tf',
-            # fn_dir_template='/home/jake/Data/APT_T',
-            fn_dir_template='/mnt/e/Control_Transition/APT/APT_Tf',
+            fn_dir_template='/home/jake/Data/APT_Tf',
+            # fn_dir_template='/mnt/e/Control_Transition/APT/APT_Tf',
             input_params_template='{p:.3f} {L} {seed} {ancilla}',
             load_data=load_pickle,
             filename=None,
@@ -45,12 +43,11 @@ for p_m in p_m_list:
         )
     data_APT=convert_pd(data_APT_dict,names=['Metrics','L','p_m','p_f','sC','sm'])
 
-# for p in p_m_list:
-    # print(p_m)
+    data_APT=data_APT.xs(L,level='L').xs(p_m,level='p_m').xs(1,level='p_f').xs('O',level='Metrics')['observations']
     sC_traj_var[(p_m,L)]=[]
     for sC in tqdm(sC_list):
         try:
-            sC_traj_var[(p_m,L)].append(trajvar(data_APT,L=L,p_m=p_m,sC=sC))
+            sC_traj_var[(p_m,L)].append(trajvar(data_APT,sC=sC))
         except:
             pass
     sC_traj_var[(p_m,L)]=np.array(sC_traj_var[(p_m,L)])
